398c69848cfd7a332fac965f7de5b1c356711ed6
fryylord <rob@mchays.com>
2022-12-22 19:59:12 -0600

    can access and add to database

--- public/js/login.js
+++ public/js/login.js
@@ -1,10 +1,11 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
+  const username = document.querySelector('#username-login').value.trim();
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
-  if (email && password) {
+  if (username && email && password) {
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
