eea666d3656b47d7e1f5c08c5f6459444ee93fcc
karasu yami <demigodlinty@gmail.com>
2023-01-04 16:52:12 -0600

    set up basic file structure

---
+++ seeds/post-seeds.js
@@ -0,0 +1,32 @@
+[
+    {
+        title:'',
+        post_content:'',
+        user_id''
+
+    },
+    {
+        title:'',
+        post_content:'',
+        user_id''
+
+    },
+    {
+        title:'',
+        post_content:'',
+        user_id''
+
+    },
+    {
+        title:'',
+        post_content:'',
+        user_id''
+
+    },
+    {
+        title:'',
+        post_content:'',
+        user_id''
+
+    },
+]
