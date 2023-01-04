f5c767e0eaaa0080fb06a5d85cd5d0d828253527
karasu yami <demigodlinty@gmail.com>
2023-01-03 21:31:02 -0600

    added appointment data

---
+++ seeds/Appointment-seeds.js
@@ -0,0 +1,10 @@
+const { Appointment } = require('../models')
+
+const AppointmentData = [
+    {
+        name: 'thomas montgomery',
+        description: 'meet up at Penn Valley Park at 3:40pm ',
+        date_created: 01-03-2023,
+        user_id: 1
+    }
+]
