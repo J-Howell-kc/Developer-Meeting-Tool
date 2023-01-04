c94ee3dcdad9a6b0b43aaf93ae982802086cc2dc
karasu yami <demigodlinty@gmail.com>
2023-01-03 21:44:33 -0600

    added layout of the seeds

--- seeds/Appointment-seeds.js
+++ seeds/Appointment-seeds.js
@@ -6,5 +6,29 @@ const AppointmentData = [
         description: 'meet up at Penn Valley Park at 3:40pm ',
         date_created: 01-03-2023,
         user_id: 1
+    },
+    {
+        name:'james Porter',
+        description:'',
+        date_created:,
+        user_id:
+    },
+    {
+        name:'',
+        description:'',
+        date_created:,
+        user_id:
+    }
+    {
+        name:'',
+        description:'',
+        date_created:,
+        user_id:
+    }
+    {
+        name:'',
+        description:'',
+        date_created:,
+        user_id:
     }
 ]
