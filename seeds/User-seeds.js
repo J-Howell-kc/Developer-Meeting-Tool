04bdfb068d9623606aaa03b075057d5f03f0514b
karasu yami <demigodlinty@gmail.com>
2023-01-03 20:12:41 -0600

    added user-seeds.js file

--- seeds/User-seeds.js
+++ seeds/User-seeds.js
@@ -1,2 +1,16 @@
 const sequelize = require = require('../config/connection')
-const {User} = require('..models/')
+const { User } = require('../models')
+
+const userData = require('./userActive.json')
+const populateData = async () => {
+  await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  process.exit(0);
+};
+
+populateData();
