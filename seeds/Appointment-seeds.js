739e4c3ed0b6a0a34430c58fcb9c84dcc46019fc
karasu yami <demigodlinty@gmail.com>
2023-01-04 16:15:01 -0600

    add more seeds alomst finish with seeds module

--- seeds/Appointment-seeds.js
+++ seeds/Appointment-seeds.js
@@ -10,20 +10,20 @@ const AppointmentData = [
     {
         name:'james Porter',
         description:'were are meeting at my place ',
-        date_created:01-03-2023,
+        date_created:10-27-2022,
         user_id:2
     },
     {
-        name:'',
-        description:'',
-        date_created:,
-        user_id:
+        name:'kimberley Evans',
+        description:'our scrum meeting will take place at the coffee shop',
+        date_created:10-27-2022,
+        user_id:3
     }
     {
-        name:'',
-        description:'',
-        date_created:,
-        user_id:
+        name:'gabriella martinez',
+        description:'tonight is our python practice be ready!',
+        date_created:10-27-2022,
+        user_id:4
     }
     {
         name:'',
