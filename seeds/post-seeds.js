1fb6625b463463d42c221468d06c44f202cc6595
karasu yami <demigodlinty@gmail.com>
2023-01-05 18:48:09 -0600

    added more to the appointment seeds will finish soon

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,31 +2,31 @@
     {
         title:'HERE I COME!',
         post_content:'sorry guys im running ',
-        user_id''
+        user_id:3
+
+    },
+    {
+        title:'anyone still coming to Penn Valley Park ',
+        post_content:'hey guys i arrived i was wondering if anyone is still coming',
+        user_id: 1
 
     },
     {
         title:'',
         post_content:'',
-        user_id''
+        user_id:''
 
     },
     {
         title:'',
         post_content:'',
-        user_id''
+        user_id:''
 
     },
     {
         title:'',
         post_content:'',
-        user_id''
-
-    },
-    {
-        title:'',
-        post_content:'',
-        user_id''
+        user_id:''
 
     },
 ]
