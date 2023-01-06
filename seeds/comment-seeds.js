f91a3c13be3f466b97a8e94dc9acabe3038825e9
karasu yami <demigodlinty@gmail.com>
2023-01-05 19:17:17 -0600

    halfway done with seeds will push soon

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,18 +3,18 @@ const { Comment } = require('../models')
 const commentData = [
     {
         comment_text:'ill be there asap!',
-        user_id:''
-        post_id
+        user_id:2,
+        post_id:1
     },
     {
-        comment_text:'',
-        user_id:''
-        post_id
+        comment_text:'im here',
+        user_id:2,
+        post_id:3
     },
     {
-        comment_text:'',
-        user_id:''
-        post_id
+        comment_text:'this park is really beautiful one of the reasons i come here lol',
+        user_id:1,
+        post_id:3
     }
     {
         comment_text:'',
