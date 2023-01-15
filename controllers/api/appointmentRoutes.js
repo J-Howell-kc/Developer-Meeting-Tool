e10b1082531faa867dac01a1dfc8d969d721b0ea
fryylord <rob@mchays.com>
2023-01-14 18:40:58 -0600

    removed some commented code

--- controllers/api/appointmentRoutes.js
+++ controllers/api/appointmentRoutes.js
@@ -69,24 +69,4 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-// router.delete('/:id', async (req, res) => {
-//   try {
-//     const appointmentData = await Appointment.destroy({
-//       where: {
-//         id: req.params.id,
-//         user_id: req.session.user_id,
-//       },
-//     });
-
-//     if (!appointmentData) {
-//       res.status(404).json({ message: 'No appointment found with this id!' });
-//       return;
-//     }
-
-//     res.status(200).json(appointmentData);
-//   } catch (err) {
-//     res.status(500).json(err);
-//   }
-// });
-
 module.exports = router;
